ebf3fdfa22d7592244df7e05df7737e1e07c7f8d
ASOwnerYT <asowner@outlook.com>
2022-09-19 09:25:20 +1200

    Remove useless code

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 let date = new Date();
-let dateTime = "";
-console.log(dateTime);
 
 window.onload = function() {
-    document.getElementById("text").innerHTML = date.toLocaleString();
+    document.getElementById("date").innerHTML = date.toDateString();
 }
 
 window.setInterval(function(){
     date = new Date();
-    document.getElementById("date").innerHTML = date.toDateString();
     document.getElementById("time").innerHTML = date.toLocaleTimeString();
 })
